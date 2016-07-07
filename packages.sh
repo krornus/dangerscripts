261994ae1650a5ae38fb7466d4d60c0fbde4d56d
krornus <krornus@gmail.com>
2016-07-07 14:28:52 -0400

    package installer

---
+++ packages.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+pacman -S i3 xorg xorg-xinit sakura firefox dmenu p7zip zsh
+
+mkdir tmp
+cd tmp
+
+git clone https://aur.archlinux.org/package-query.git
+cd package-query
+makepkg -si
+cd ..
+git clone https://aur.archlinux.org/yaourt.git
+cd yaourt
+makepkg -si
+cd ..
+
+cd ..
+rm rf tmp
+
+echo '[[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && exec startx' > $HOME/.zprofile
+echo '#!/bin/bash' > $HOME/.xinitrc
+echo 'exec i3' >> $HOME/.xinitrc
